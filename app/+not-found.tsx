5c362f2a644daa069650576a71325f5c594dbae2
Raj-venom <rajsingh.rsr9824@gmail.com>
2025-02-04 01:40:59 +0545

    add initial setup for NativeWind; include global styles, layout, and authentication screens

---
+++ app/+not-found.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NoFound = () => {
+  return (
+    <View>
+      <Text>NoFound</Text>
+    </View>
+  )
+}
+
+export default NoFound
