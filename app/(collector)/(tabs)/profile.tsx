78fc55698e028f1799e3c3dded0afad7220f2280
Raj-venom <rajsingh.rsr9824@gmail.com>
2025-04-14 17:34:29 +0545

    add logout functionality to profile component; implement error handling and user feedback

--- app/(collector)/(tabs)/profile.tsx
+++ app/(collector)/(tabs)/profile.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert, TouchableOpacity } from 'react-native'
 import React from 'react'
+import collectorAuthService from '@/services/collector/collectorAuth'
+import { useDispatch } from 'react-redux'
+import { logout } from '@/contexts/features/auth/authSlice'
+import { router } from 'expo-router'
 
 const profile = () => {
+  const dispatch = useDispatch()
+  const logoutCall = async () => {
+
+    try {
+      const response = await collectorAuthService.logout()
+      console.log(response)
+      if (response.success) {
+        dispatch(logout())
+        router.replace('/(auth)/sign-in')
+      } else {
+        {
+          Alert.alert('Error', response.message)
+        }
+
+      }
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error', 'Failed to logout. Please try again.')
+
+    }
+  }
+
   return (
     <View>
-      <Text>profile</Text>
+      <TouchableOpacity onPress={logoutCall}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
